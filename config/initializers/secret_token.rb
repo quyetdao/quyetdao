cbea677c06e2d981f3343f68673ffc9833b3aec9
quyetdao <quyetdao@cnc.vn>
2012-12-20 09:04:42 +0700

    create new reposity

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quyetdao::Application.config.secret_token = '4b06a9ba621548c5822a4ea0380bce3d849d87cafe9833833e0e8dbc59903558d7321ab06d3a90ad7ed37323a32b9fa4431ca2717c7e688485521f51f092c508'
